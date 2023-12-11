05d6194c94d59d0c714acf9a0f16f322244673e0
mukeshxplor <mukesh.dode@xplortechnologies.com>
2023-12-11 11:08:37 +0530

    some modification are add

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,41 +3,91 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\category;
 
 class CategoryController extends Controller
 {
-    //
-
+   
+   /**
+     * All the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $Categories
+     * @return \Illuminate\Http\Response
+     */
     public function index(){
         $categories = category::latest()->paginate(10);
         return response()->json($categories);
     }
 
+    /**
+     * Add the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $Categories
+     * @return \Illuminate\Http\Response
+     */
+
     public function store(Request $request){
-        $input = $request->all();
+     
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+           
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+                'status' => 404,
+            ],201);
+        }
+
+        $input = $request->all(); 
         category::create($input);
-        return response()->json(['message'=>'Categories created success'],201);
+        return response()->json(['message'=>'Category created success'],201);
     }
 
-    public function show($Category $category){
-        return [
-            "status" => 1,
-            "data" =>$category
-        ];
+    /**
+     * Get the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $Categories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category){
+        return response()->json([
+                "status" => 1,
+                "data" =>$category
+        ],201);
     }
 
+     /**
+     * Update the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $Categories
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, Category $category){
         $request->validate([
             'name' => 'required'
         ]);
+
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+           
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+                'status' => 404,
+            ],201);
+        }
+
         $category->update($request->all());
 
-        return [
+        return response()->json([
             "status" => 1,
             "data" => $category,
             "msg" => "category updated successfully"
-        ];
+        ],201);
     }
 
     /**
@@ -49,10 +99,10 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         $category->delete();
-        return [
+        return response()->json([
             "status" => 1,
-            "data" => $Category,
+            "data" => $category,
             "msg" => "Category deleted successfully"
-        ];
+        ],201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('/greeting', function () {
+    return 'Hello World';
 });
+
+Route::resource('/category', CategoryController::class);
+// Route::resource('/product', CategoryController::class);
